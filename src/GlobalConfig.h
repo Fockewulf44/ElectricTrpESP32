387958a29f77ab914e96b17496a965c580466ad3
Fockewulf44 <cruel.programmer@gmail.com>
2020-08-30 21:41:51 -0700

    Password change

--- src/GlobalConfig.h
+++ src/GlobalConfig.h
@@ -1,7 +1,7 @@
 struct GlobalConfig
 {
     const char *ssid = "Sonoma #3 Google Wifi";
-    const char *password = "k4kd99ola7";
+    const char *password = "k4kd99dola7";
 };
 
 GlobalConfig Config;
